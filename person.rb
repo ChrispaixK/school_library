e54f5b2d0222ae5756c1f3ee68fc277beb96f081
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-26 19:43:25 +0200

    Fix linters error and update ruby

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'unknown', parent_permission : true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Time.new.to_i
     @name = name
     @age = age
