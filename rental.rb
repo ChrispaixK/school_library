8a90e57ee8f13045030ca2f6431aba677158a262
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-09-28 17:41:45 +0200

    Implement requested changes

--- rental.rb
+++ rental.rb
@@ -7,7 +7,7 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @book = book
-    book.rental << self
+    book.rentals << self
     @person = person
     person.rentals << self
   end
