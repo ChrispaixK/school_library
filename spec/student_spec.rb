1687c11e4a19db8704fd25dff080b8d179ecf382
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-06 09:21:20 +0100

    Test the name of the student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,20 +4,11 @@ describe Student do
     before(:each) do
       @student = Student.new(30, 'Dennis')
     end
-    it 'Should return the specialization of the teacher as a dentist' do
+    it 'Should return the age of the teacher as a 30' do
       expect(@student.age).to eq 30
     end
-    # it 'Should return the age of the teacher as 30' do
-    #   expect(@student.age).to eq 30
-    # end
-    # it 'Should return the name of the teacher as Samuel' do
-    #   expect(@student.name).to eq 'Samuel'
-    # end
-    # it 'Check if teacher is an instance of the Teacher class' do
-    #   expect(@student).to be_an_instance_of Student
-    # end
-    # it 'Check if teacher has parent permission' do
-    #   expect(@student.parent_permission).to eq true
-    # end
-  end
+    it 'Should return the name of the student as Dennis' do
+      expect(@student.name).to eq 'Dennis'
+    end
+   end
   
