6789fdba8f2e5fabe17833c828ca40a9f2370089
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-10-06 09:02:36 +0100

    Add test for the age of the teacher

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -7,4 +7,7 @@ describe Teacher do
     it "Should return the specialization of the teacher as a dentist" do
         expect(@teacher.specialization).to eq 'Dentist'
     end
+    it "Should return the age of the teacher as 30" do
+        expect(@teacher.age).to eq 30
+    end
 end
